2c2488b8f48cd7bae8d1a0d372e48d14fd072805
CodeCrafterNishit <nishitjain816@gmail.com>
2026-06-21 23:38:12 +0530

    Complete Function related challenge problem

---
+++ Functions&Methods/GCD.java
@@ -0,0 +1,18 @@
+public class GCD {
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 24;
+        int num2 = 36;
+
+        System.out.println("GCD = " + gcd(num1, num2));
+    }
+}
