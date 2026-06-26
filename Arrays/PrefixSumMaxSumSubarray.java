ef30674e531a8e7f95588c526d3508a8042bcb9b
CodeCrafterNishit <nishitjain816@gmail.com>
2026-06-26 23:42:56 +0530

    Added Max Sum Subarray using PrefixSum Method

---
+++ Arrays/PrefixSumMaxSumSubarray.java
@@ -0,0 +1,26 @@
+public class PrefixSumMaxSumSubarray {
+    public static void PrefixSum_MaxSumSubarray(int arr[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max sum : " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, -2, 5, 6, 3 };
+        PrefixSum_MaxSumSubarray(arr);
+    }
+}
