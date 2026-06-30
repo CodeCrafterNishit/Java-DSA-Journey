66ddacc8438178a8c32bfb5cf296bce72cce7790
CodeCrafterNishit <nishitjain816@gmail.com>
2026-06-30 23:21:40 +0530

    Solved Practice Problems of Arrays

---
+++ Arrays/HW1.java
@@ -0,0 +1,21 @@
+package Arrays;
+public class HW1 {
+    public static Boolean CheckOccurence(int nums[]) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1,4,7,1,4,1  };
+        System.out.println(CheckOccurence(nums));
+    }
+}
+
+//tc-->O(n2)
