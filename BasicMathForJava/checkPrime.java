25a3ea04b87df1c29bd764f43af5f7561457c8de
CodeCrafterNishit <nishitjain816@gmail.com>
2026-07-17 23:42:18 +0530

    Solved Basic Math Problems

--- BasicMathForJava/checkPrime.java
+++ BasicMathForJava/checkPrime.java
@@ -9,9 +9,18 @@ public class checkPrime {
         }
         return true;
     }
+      public static boolean CheckPrimeNum2(int n) {
+        for (int i = 2; i*i <= n - 1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         int n = 101;
         System.out.println(CheckPrimeNum(n));
+        System.out.println(CheckPrimeNum2(n));
     }
 }
