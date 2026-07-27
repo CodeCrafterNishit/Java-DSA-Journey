3e7bd9bfbcb608c5129e7f0c337a5a88af93838c
CodeCrafterNishit <nishitjain816@gmail.com>
2026-07-27 21:33:56 +0530

    Encapsulation In OOPS

--- OOPS/App.java
+++ OOPS/App.java
@@ -2,24 +2,29 @@ package OOPS;
 
 public class App {
     public static void main(String[] args) {
+        // new object
         Student A = new Student();
         A.id = 1;
         A.name = "Nishit";
         A.nos = 5;
-        A.age = 19;
+        // A.age = 19;
         System.out.println(A.id);
-        System.out.println(A.age);
+        // System.out.println(A.age);
         System.out.println(A.name);
         System.out.println(A.nos);
         A.bunk();
         A.study();
         A.sleep();
+        // A.gfChatting();
 
-        //parameterized ctor
-        Student A1 = new Student(1,19,"Nishit",5);
-         System.out.println(A1.id);
-        System.out.println(A1.age);
+        // parameterized ctor
+        Student A1 = new Student(1, 19, "Nishit", 5, "Tina");
+        System.out.println(A1.id);
+        System.out.println(A1.getAge());
+        A1.setAge(22);
+        System.out.println(A1.getAge());
         System.out.println(A1.name);
         System.out.println(A1.nos);
+        System.out.println(A1.getName());
     }
 }
--- OOPS/Student.java
+++ OOPS/Student.java
@@ -3,34 +3,56 @@ package OOPS;
 //class is a blueprint
 public class Student {
 
-    //attributes
+    // attributes
     public int id;
     public String name;
-    public int age;
+    private int age;
     public int nos;
+    private String gf;
 
-    //default ctor
-    public Student(){
+    public String getName() {
+        return this.gf;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int a) {
+        if (a<100)
+            this.age = a;
+        return;
+    }
+
+    // default ctor
+    public Student() {
         System.out.println("student default constructor called");
     }
 
-    //parameterized ctor
-    public Student(int id,int age,String name,int nos){
+    // parameterized ctor
+    public Student(int id, int age, String name, int nos, String gf) {
         System.out.println("Parameterized constructor called");
         this.id = id;
         this.age = age;
         this.name = name;
         this.nos = nos;
+        this.gf = gf;
     }
 
-    //Methods / behaviours
-    public void study(){
+    // Methods / behaviours
+    public void study() {
         System.out.println(name + " is Studying");
-    } 
-    public void sleep(){
+    }
+
+    public void sleep() {
         System.out.println(name + " is Sleeping");
     }
-    public void bunk(){
+
+    public void bunk() {
         System.out.println(name + " is Bunking");
     }
+
+    // private void gfChatting() {
+    // System.out.println("chatting with " + gf);
+    // }
 }
