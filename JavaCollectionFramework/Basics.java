0950871e1d22f3afcf1e7593bac27184d9124e28
CodeCrafterNishit <nishitjain816@gmail.com>
2026-08-11 23:51:15 +0530

    Practiced LinkedList Basic Methods

--- JavaCollectionFramework/Basics.java
+++ JavaCollectionFramework/Basics.java
@@ -1,14 +1,16 @@
 package JavaCollectionFramework;
+
 import java.util.ArrayList;
 // import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
-public class Basics{
+public class Basics {
     public static void main(String[] args) {
-        //collection or list both are interface 
-        //arraylist is a concrete class 
+        // collection or list both are interface
+        // arraylist is a concrete class
         // ArrayList<Integer> arr = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         list.add(10);
@@ -32,9 +34,9 @@ public class Basics{
         System.out.println(list2);
         list2.clear();
         System.out.println(list2);
-        //iterator
-        Iterator <Integer> iterator = list.iterator(); 
-        while(iterator.hasNext()){
+        // iterator
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
             System.out.println("Element : " + iterator.next());
         }
         List<Integer> list3 = new ArrayList<>();
@@ -45,10 +47,10 @@ public class Basics{
         System.out.println(list3);
         System.out.println(list3.get(2));
         System.out.println(list3.get(3));
-        list3.set(0,10);
+        list3.set(0, 10);
         System.out.println(list3);
-        Object[] arr =list3.toArray();
-        for(Object obj:arr){
+        Object[] arr = list3.toArray();
+        for (Object obj : arr) {
             System.out.println("Element : " + obj);
         }
         System.out.println(list3.contains(10));
@@ -63,7 +65,7 @@ public class Basics{
         list4.add(30);
         list4.add(40);
         list4.add(50);
-        ArrayList<Integer> newList = (ArrayList<Integer>)list4.clone();
+        ArrayList<Integer> newList = (ArrayList<Integer>) list4.clone();
         System.out.println(newList);
         System.out.println(newList.isEmpty());
         System.out.println(newList.indexOf(40));
@@ -73,5 +75,45 @@ public class Basics{
         System.out.println(marks.size());
 
         // Collection<Integer> collection = new ArrayList<>();
+
+        // LinkedList
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(100);
+        ll.add(101);
+        System.out.println(ll);
+        list.addAll(ll);
+        System.out.println(list);
+        list.remove(4);
+        System.out.println(list);
+        list.removeAll(ll);
+        System.out.println(list);
+        ll.add(100);
+        ll.add(101);
+        System.out.println(ll.size());
+        System.out.println(ll);
+        list2.clear();
+        System.out.println(ll);
+        // iterator
+        Iterator<Integer> iterator1 = ll.iterator();
+        while (iterator1.hasNext()) {
+            System.out.println("Element : " + iterator1.next());
         }
+        ll.addFirst(99);
+        ll.addLast(98);
+        System.out.println(ll);
+        System.out.println(ll.getFirst());
+        System.out.println(ll.getLast());
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println(ll);
+        System.out.println("----------");
+        System.out.println(ll.peek());
+        System.out.println(ll);
+        System.out.println(ll.poll());
+        System.out.println(ll);
+
+        System.out.println(ll.offer(101));
+        System.out.println(ll);
+
+    }
 }
